8de128bb4a160fd5a133107cc8f505f118b4f198
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-18 10:33:48 +0200

    updated
    after @virtullight comments

--- leap.py
+++ leap.py
@@ -1,13 +1,14 @@
-# Function definition is here
-def is_leap_year( year ):
-   if (year%4 == 0 and year%100 != 0) or year%400 ==0:
-       print str(year) + " is a leap year"
-   else:
-        print str(year) + " is NOT a leap year"
-   return;
+def is_leap_year(year):
+    if (year%4 == 0 and year%100 != 0) or year%400 == 0:
+#https://www.python.org/dev/peps/pep-0008/#indentation
+        print(str(year) + " is a leap year")
+    else:
+        print(str(year) + " is NOT a leap year")
+    return
 
 #For example, 1997 is not a leap year, but 1996 is.  1900 is not a leap year, but 2000 is.
-is_leap_year (1997)
-is_leap_year (1996)
-is_leap_year (1900)
-is_leap_year (2000)
+#https://www.python.org/dev/peps/pep-0008/#pet-peeves
+is_leap_year(1997)
+is_leap_year(1996)
+is_leap_year(1900)
+is_leap_year(2000)
