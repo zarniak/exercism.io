fcdc6c455e9941b990805eab7d058a5d40cfef05
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-19 18:33:10 +0200

    checked with pep8
    @virtuallight
    checked the code with http://pep8online.com and such comment occurs: "block comment should start with '# '"

--- leap.py
+++ leap.py
@@ -1,12 +1,12 @@
 def is_leap_year(year):
-    if (year%4 == 0 and year%100 != 0) or year%400 == 0:
-#https://www.python.org/dev/peps/pep-0008/#indentation
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        #https://www.python.org/dev/peps/pep-0008/#indentation
         print(str(year) + " is a leap year")
     else:
         print(str(year) + " is NOT a leap year")
-    return
 
-#For example, 1997 is not a leap year, but 1996 is.  1900 is not a leap year, but 2000 is.
+#For example, 1997 is not a leap year, but 1996 is.
+#1900 is not a leap year, but 2000 is.
 #https://www.python.org/dev/peps/pep-0008/#pet-peeves
 is_leap_year(1997)
 is_leap_year(1996)
