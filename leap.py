a1bc157cb1d1dae71716375f58198f665e635c2c
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-20 18:55:24 +0200

    leap-test modification
    @virtuallight
    changed to retunr true or false
    space in comment added

--- leap.py
+++ leap.py
@@ -1,14 +1,14 @@
 def is_leap_year(year):
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-        #https://www.python.org/dev/peps/pep-0008/#indentation
-        print(str(year) + " is a leap year")
+        # https://www.python.org/dev/peps/pep-0008/#indentation
+        return True
     else:
-        print(str(year) + " is NOT a leap year")
+        return False
 
-#For example, 1997 is not a leap year, but 1996 is.
-#1900 is not a leap year, but 2000 is.
-#https://www.python.org/dev/peps/pep-0008/#pet-peeves
-is_leap_year(1997)
-is_leap_year(1996)
-is_leap_year(1900)
-is_leap_year(2000)
+# For example, 1997 is not a leap year, but 1996 is.
+# 1900 is not a leap year, but 2000 is.
+# https://www.python.org/dev/peps/pep-0008/#pet-peeves
+print(is_leap_year(1997))
+print(is_leap_year(1996))
+print(is_leap_year(1900))
+print(is_leap_year(2000))
