c0e761a52a3593124463803d2d933ae0160f66f2
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-21 15:18:20 +0200

    update rna transcription
    checked with pep8
    chekced with unit test
    @virtuallight - dictionary solution :) do you approve?

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,2 +1,11 @@
 def to_rna(dna_strand):
-    pass
+    rna = ''
+    for x in dna_strand:
+        rna = rna + {
+            'G': 'C',
+            'C': 'G',
+            'T': 'A',
+            'A': 'U'
+        }[x]
+    return rna
+print(to_rna('ACGTGGTCTTAA'))
