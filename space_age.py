91106062f3fbe6566bb205497e392d8e1bc9df5a
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-21 11:48:12 +0200

    added loop
    @viruallight
    i have added loop - wokr fine, but have issue with formating line 10 i get such error "ValueError: cannot switch from automatic field numbering to manual field specification" when try to input "0:.2f in second bracket like
    print('{} = {0:.2f} years old'.format(planet[0], seconds/60/60/24/365*planet[1]))

--- space_age.py
+++ space_age.py
@@ -1,12 +1,10 @@
 class SpaceAge(object):
     def __init__(self, seconds):
-        print('Earth = ' + '{0:.2f}'.format(seconds/60/60/24/365) + ' years old')
-        print('Mercury = ' + '{0:.2f}'.format(seconds/60/60/24/365*0.2408467) + ' years old')
-        print('Venus = ' + '{0:.2f}'.format(seconds/60/60/24/365*0.61519726) + ' years old')
-        print('Mars = ' + '{0:.2f}'.format(seconds/60/60/24/365*1.8808158) + ' years old')
-        print('Jupiter = ' + '{0:.2f}'.format(seconds/60/60/24/365*11.862615) + ' years old')
-        print('Saturn = ' + '{0:.2f}'.format(seconds/60/60/24/365*29.447498) + ' years old')
-        print('Uranus = ' + '{0:.2f}'.format(seconds/60/60/24/365*84.016846) + ' years old')
-        print('Mercury = ' + '{0:.2f}'.format(seconds/60/60/24/365*164.79132) + ' years old')
+        planets = [
+            ['Earth', 1], ['Mercury', 0.2408467], ['Venus', 0.61519726],
+            ['Mars', 1.8808158], ['Jupiter', 11.862615], ['Saturn', 29.447498],
+            ['Uranus', 84.016846], ['Neptune', 164.79132]]
+        for planet in planets:
+            print('{} = {0:.2f} years old'.format(planet[0], seconds/60/60/24/365*planet[1]))
 
 SpaceAge(71997456)
