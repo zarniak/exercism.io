985231481df1fc2d86560aaaecdd9520de75b546
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-21 14:54:28 +0200

    updated gigasecond
    checked with pep8
    checked with unittest, but @virtuallight please see unittest - would it be working with timedelta if there is no "timedelta" import?

--- gigasecond.py
+++ gigasecond.py
@@ -1,2 +1,13 @@
+from datetime import datetime
+from datetime import timedelta
+
+
 def add_gigasecond(birth_date):
-    pass
+    return birth_date + timedelta(seconds=10**9)
+
+print(add_gigasecond(datetime(2011, 4, 25)))
+print(add_gigasecond(datetime(1977, 6, 13)))
+print(add_gigasecond(datetime(1959, 7, 19)))
+print(add_gigasecond(datetime(2015, 1, 24, 22, 0, 0)))
+print(add_gigasecond(datetime(2015, 1, 24, 23, 59, 59)))
+print(add_gigasecond(datetime(1984, 2, 11)))
