9039d3fd721539b8ad0590fda92a6f5ff59d49ac
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-21 14:19:47 +0200

    update of sqaures
    checked with pep8
    checked with unittest
    @virtuallight do you approve? :)

--- difference_of_squares.py
+++ difference_of_squares.py
@@ -1,10 +1,25 @@
 def square_of_sum(count):
-    pass
+    s = 0
+    for x in range(1, count+1):
+        s = s + x
+    return s**2
 
 
 def sum_of_squares(count):
-    pass
+    s = 0
+    for x in range(1, count+1):
+        s = s + x ** 2
+    return s
 
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)
+print(square_of_sum(1))
+print(sum_of_squares(1))
+print(difference(1))
+print(square_of_sum(5))
+print(sum_of_squares(5))
+print(difference(5))
+print(square_of_sum(100))
+print(sum_of_squares(100))
+print(difference(100))
