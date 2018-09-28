e0a092052e48794fa2acc5ab687b35687b75173f
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-28 17:41:10 +0200

    update bob
    @virtuallight i get pep8 errors "multiple statements on one line (colon)", what's the matter? how to fix that?

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,6 @@
 def hey(phrase):
-    pass
+    if phrase.isspace() or not phrase: return 'Fine. Be that way!'
+    if phrase.strip().find('?')+1 == len(phrase.strip()) and not phrase.isupper(): return 'Sure.'
+    if phrase.strip().find('?')+1 == len(phrase.strip()) and phrase.isupper(): return 'Calm down, I know what Im doing!'
+    if phrase.strip().find('!') and phrase.isupper(): return 'Whoa, chill out!'
+    return 'Whatever.'
