b8b1e924e1ccebe4dab18e677ecf43e0869a41ed
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-28 11:28:30 +0200

    Update gigasecond.py

--- gigasecond.py
+++ gigasecond.py
@@ -1,13 +1,5 @@
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 def add_gigasecond(birth_date):
     return birth_date + timedelta(seconds=10**9)
-
-print(add_gigasecond(datetime(2011, 4, 25)))
-print(add_gigasecond(datetime(1977, 6, 13)))
-print(add_gigasecond(datetime(1959, 7, 19)))
-print(add_gigasecond(datetime(2015, 1, 24, 22, 0, 0)))
-print(add_gigasecond(datetime(2015, 1, 24, 23, 59, 59)))
-print(add_gigasecond(datetime(1984, 2, 11)))
