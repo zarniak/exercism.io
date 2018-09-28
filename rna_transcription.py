82666369bbad1c57f9a41cdd6368974030839eb4
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-28 13:07:54 +0200

    one line solution
    @virtuallight solution with use of sum of squares solution (modified) and some google :)

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,11 +1,10 @@
-def to_rna(dna_strand):
-    rna = ''
-    for x in dna_strand:
-        rna = rna + {
+dna_to_rna = {
             'G': 'C',
             'C': 'G',
             'T': 'A',
             'A': 'U'
-        }[x]
-    return rna
-print(to_rna('ACGTGGTCTTAA'))
+        }
+
+
+def to_rna(dna_strand):
+    return ''.join([dna_to_rna[x] for x in dna_strand])
