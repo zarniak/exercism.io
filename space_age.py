adeb2b834c3d8a8aea97e64679ced223d06e1401
zarniak <42268065+zarniak@users.noreply.github.com>
2018-09-28 14:11:07 +0200

    updated but get the error
    @virtuallight i think i get it straight but still gets an error
    
    File "/home/main.py", line 34, in on_neptune                                                                                                              
        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Neptune']                                                                                       
    AttributeError: 'int' object has no attribute 'seconds'

--- space_age.py
+++ space_age.py
@@ -1,33 +1,37 @@
 class SpaceAge(object):
-    __init__(self, seconds):
+    SECONDS_IN_EARTH_YEAR = 60 * 60 * 24 * 365
+	
+    def __init__(self, seconds):
         self.seconds = seconds
         self.planets = {
             'Earth': 1, 'Mercury': 0.2408467, 'Venus': 0.61519726,
             'Mars': 1.8808158, 'Jupiter': 11.862615, 'Saturn': 29.447498,
             'Uranus': 84.016846, 'Neptune': 164.79132
         }
-
+        
     def on_mercury(self):
-        return self.seconds/60/60/24/365*self.planets.get('Mercury')
+       return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Mercury']
 
     def on_venus(self):
-        return self.seconds/60/60/24/365*self.planets.get('Venus')
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Venus']
 
     def on_earth(self):
-        return self.seconds/60/60/24/365*self.planets.get('Earth')
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Earth']
 
     def on_mars(self):
-        return self.seconds/60/60/24/365*self.planets.get('Mars')
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Mars']
 
     def on_jupiter(self):
-        return self.seconds/60/60/24/365*self.planets.get('Jupiter')
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Jupiter']
 
     def on_saturn(self):
-        return self.seconds/60/60/24/365*self.planets.get('Saturn')
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Saturn']
 
     def on_uranus(self):
-        return self.seconds/60/60/24/365*self.planets.get('Uranus')
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Uranus']
 
     def on_neptune(self):
-        return self.seconds/60/60/24/365*self.planets.get('Neptune')
-SpaceAge(71997456)
+        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Neptune']
+
+
+SpaceAge.on_neptune(71997456)
