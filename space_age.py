c8e118641d7755be00071d4814b80ea20d7eb3e8
Mateusz Jankowski <mjankowski@laterpay.net>
2018-10-01 22:03:08 +0200

    Define constants at the top of the file

--- space_age.py
+++ space_age.py
@@ -1,6 +1,17 @@
+SECONDS_IN_EARTH_YEAR = 60 * 60 * 24 * 365
+PLANETS_EARTH_YEAR_LENGTH = {
+    'Earth': 1,
+    'Mercury': 0.2408467,
+    'Venus': 0.61519726,
+    'Mars': 1.8808158,
+    'Jupiter': 11.862615,
+    'Saturn': 29.447498,
+    'Uranus': 84.016846,
+    'Neptune': 164.79132,
+}
+
 class SpaceAge(object):
-    SECONDS_IN_EARTH_YEAR = 60 * 60 * 24 * 365
-	
+
     def __init__(self, seconds):
         self.seconds = seconds
         self.planets = {
