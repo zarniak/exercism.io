a38a72a3cb903277b813c1bcdfdce3df1e43e86c
Mateusz Jankowski <mjankowski@laterpay.net>
2018-10-01 22:05:04 +0200

    Pre-compute age on each planet to facilitite results reuse

--- space_age.py
+++ space_age.py
@@ -14,35 +14,31 @@ class SpaceAge(object):
 
     def __init__(self, seconds):
         self.seconds = seconds
-        self.planets = {
-            'Earth': 1, 'Mercury': 0.2408467, 'Venus': 0.61519726,
-            'Mars': 1.8808158, 'Jupiter': 11.862615, 'Saturn': 29.447498,
-            'Uranus': 84.016846, 'Neptune': 164.79132
-        }
-        
+        years = self.seconds / SECONDS_IN_EARTH_YEAR
+        self.planets_age = {k: years * v for (k, v) in PLANETS_EARTH_YEAR_LENGTH.items()}
+
     def on_mercury(self):
-       return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Mercury']
+       return self.planets_age['Mercury']
 
     def on_venus(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Venus']
+        return self.planets_age['Venus']
 
     def on_earth(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Earth']
+        return self.planets_age['Earth']
 
     def on_mars(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Mars']
+        return self.planets_age['Mars']
 
     def on_jupiter(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Jupiter']
+        return self.planets_age['Jupiter']
 
     def on_saturn(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Saturn']
+        return self.planets_age['Saturn']
 
     def on_uranus(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Uranus']
+        return self.planets_age['Uranus']
 
     def on_neptune(self):
-        return self.seconds/SECONDS_IN_EARTH_YEAR*self.planets['Neptune']
-
+        return self.planets_age['Neptune']
 
 SpaceAge.on_neptune(71997456)
