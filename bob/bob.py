b8467e056a89e326beb1c31da604df73a87512c0
zarniak <42268065+zarniak@users.noreply.github.com>
2018-10-03 13:51:27 +0200

    Update bob.py
    @ virtuallight i wanted to shorten 3-7 lines like below, but gets error like below the code
        if phrase.strip().find('?')+1 == len(phrase.strip()):
            if phrase.isupper():
    		    return 'Calm down, I know what Im doing!'
    		elif not phrase.isupper():
               return 'Sure.'
    
     File "/home/main.py", line 6                                                                                                                              
        return 'Calm down, I know what Im doing!'                                                                                                               
                                                ^                                                                                                               
    TabError: inconsistent use of tabs and spaces in indentation

--- bob/bob.py
+++ bob/bob.py
@@ -1,6 +1,10 @@
 def hey(phrase):
-    if phrase.isspace() or not phrase: return 'Fine. Be that way!'
-    if phrase.strip().find('?')+1 == len(phrase.strip()) and not phrase.isupper(): return 'Sure.'
-    if phrase.strip().find('?')+1 == len(phrase.strip()) and phrase.isupper(): return 'Calm down, I know what Im doing!'
-    if phrase.strip().find('!') and phrase.isupper(): return 'Whoa, chill out!'
+    if phrase.isspace() or not phrase:
+        return 'Fine. Be that way!'
+    if phrase.strip().find('?')+1 == len(phrase.strip()) and not phrase.isupper():
+        return 'Sure.'
+    if phrase.strip().find('?')+1 == len(phrase.strip()) and phrase.isupper():
+        return 'Calm down, I know what Im doing!'
+    if phrase.strip().find('!') and phrase.isupper():
+        return 'Whoa, chill out!'
     return 'Whatever.'
