9dd8c70ca394c222f1af5a7de2ffa9393f362894
zarniak <42268065+zarniak@users.noreply.github.com>
2018-10-18 14:07:58 +0200

    Update prime factors
    @virtuallight nice one - check it out!

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,10 @@
 def prime_factors(natural_number):
-    pass
+    list = []
+    x = 2
+    while x <= natural_number:
+        if natural_number % x == 0:
+            list.append(x)
+            natural_number = natural_number/x
+        else:
+            x += 1
+    return list
