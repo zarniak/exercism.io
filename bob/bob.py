e828fa194cc0e169c986dc4a41fff41ab7f76f94
zarniak <42268065+zarniak@users.noreply.github.com>
2018-10-31 09:55:48 +0100

    Update bob.py

--- bob/bob.py
+++ bob/bob.py
@@ -2,10 +2,10 @@ def hey(phrase):
     phrase_stripped = phrase.strip()
     if phrase_stripped.isspace() or not phrase_stripped:
         return 'Fine. Be that way!'
-    if phrase_stripped.endswith('?') and not phrase_stripped.isupper():
-        return 'Sure.'
     if phrase_stripped.endswith('?') and phrase_stripped.isupper():
         return "Calm down, I know what I'm doing!"
+    if phrase_stripped.endswith('?'):
+        return 'Sure.'
     if phrase_stripped.isupper():
         return 'Whoa, chill out!'
     return 'Whatever.'
