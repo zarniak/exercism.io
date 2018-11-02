96cbba76530bf51c35c3cc70e9f773ed1b32e503
zarniak <42268065+zarniak@users.noreply.github.com>
2018-11-02 13:38:59 +0100

    Update allergies

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,11 +1,35 @@
+ALLERGENS = {
+    1: 'eggs',
+    2: 'peanuts',
+    4: 'shellfish',
+    8: 'strawberries',
+    16: 'tomatoes',
+    32: 'chocolate',
+    64: 'pollen',
+    128: 'cats',
+}
+
 class Allergies(object):
 
     def __init__(self, score):
-        pass
+        self.score = score
 
     def is_allergic_to(self, item):
-        pass
+        self.item = item
+        #item in allergen_list
 
-    @property
+#    @property
     def lst(self):
-        pass
+        allergen_list = []
+        score = self.score
+        factors = 7
+        while factors >= 0:
+            if score >= 2**factors:
+                score -= 2**factors
+                allergen_list.append(ALLERGENS.get(2**factors))
+            factors -= 1
+        return allergen_list
+        
+        
+print(Allergies(5).lst())
+print(Allergies(5).is_allergic_to('eggs'))
