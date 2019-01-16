038c367a1454d1b8c6c049d63e01213d1b165fe2
zarniak <42268065+zarniak@users.noreply.github.com>
2019-01-16 11:58:26 +0100

    Update hamming.py

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,5 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('a')
-    else:
-        return sum(char_1 != char_2 for char_1, char_2 in zip(strand_a, strand_b))
+    return sum(char_1 != char_2 for char_1, char_2 in zip(strand_a, strand_b))
